c315c290e48b631529e8147ff9f06d3fb277bc42
PGgamer2 <38359416+PGgamer2@users.noreply.github.com>
2021-01-26 10:22:57 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Rickroll Detector
+Detect any Rickroll in some seconds!
+
+## What's a Rickroll
+Everybody knows what it is. There's also a [Wikipedia](https://wikipedia.org/wiki/Rickrolling) and a [Know Your Meme](https://knowyourmeme.com/memes/rickroll) article.
---
+++ main.js
@@ -0,0 +1,38 @@
+function readJSONfile(file, callback) {
+    var rawFile = new XMLHttpRequest();
+    rawFile.overrideMimeType("application/json");
+    rawFile.open("GET", file, true);
+    rawFile.onreadystatechange = function() {
+        if (rawFile.readyState === 4 && rawFile.status == "200") {
+            callback(rawFile.responseText);
+        }
+    }
+    rawFile.send(null);
+}
+
+function youtubeParser(url) {
+    var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/;
+    var match = url.match(regExp);
+    return (match&&match[7].length==11)? match[7] : false;
+}
+
+function isThisArickroll(rickLink) {
+	var rickYTid = youtubeParser(rickLink);
+	readJSONfile("rickrolls.json", function(text){
+		var totalLinks = JSON.parse(text);
+		for (var i = 0; i < totalLinks.YouTube.length; i++) {
+			if (rickYTid == youtubeParser(totalLinks.YouTube[i])) {
+				document.getElementById("rickornot").innerHTML = "A Rickroll has been detected!";
+				document.getElementById("rickornot").style.color = "red";
+				return;
+			}
+		}
+		
+		document.getElementById("rickornot").innerHTML = "This URL seems Rick-free! You're safe.";
+		document.getElementById("rickornot").style.color = "white";
+	});
+}
+
+document.getElementById("rickbutton").addEventListener("click", function() {
+	isThisArickroll(document.getElementById("rickLinkInput").value);
+});
